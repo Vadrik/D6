42aa29b5a52c27e43edb6aaae852a89c6d82baf8
Vadrik <kovtun.vadim94@gmail.com>
2023-02-07 02:08:59 +0300

    Initial commit

--- D6/simpleapp/templatetags/custom_filters.py
+++ D6/simpleapp/templatetags/custom_filters.py
@@ -8,13 +8,12 @@ register = template.Library()
 # Регистрируем наш фильтр под именем currency, чтоб Django понимал,
 # что это именно фильтр для шаблонов, а не простая функция.
 @register.filter()
-def currency(value):
+def censor(value):
     try:
-        for i in value:
-            if "редиска" in value:
-                value = value.replace('редиска', 'р******')
-                return value
-            else:
-                return value
+        if "редиска" in value:
+            value = value.replace('редиска', 'р******')
+            return value
+        else:
+            return value
     except:
         raise TypeError("Функция принимает только строки")
